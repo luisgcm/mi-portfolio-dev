1f8918e9a5b9342737f2bef549534d0dfda8704a
luisgcm <luisgcm74985929@gmail.com>
2026-03-06 16:07:53 -0500

    Agregar Menú Mobile

--- script.js
+++ script.js
@@ -70,3 +70,23 @@ form.addEventListener('submit', function(evento) {
   evento.preventDefault();
   validarFormulario();
 });
+
+
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+navToggle.addEventListener('click', function() {
+  navLinks.classList.toggle('active');
+});
+
+document.querySelectorAll('.nav-links a').forEach(link => {
+  link.addEventListener('click', function() {
+    navLinks.classList.remove('active');
+  });
+});
+
+document.addEventListener('click', function(event) {
+  if (!event.target.closest('nav')) {
+    navLinks.classList.remove('active');
+  }
+});
